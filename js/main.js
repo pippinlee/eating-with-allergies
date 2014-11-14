71f792234a53c7167769d35ca3e926e221dc960a
Pippin Lee <pippinblee@gmail.com>
2014-11-14 17:37:08 -0500

    add dropdown

--- js/main.js
+++ js/main.js
@@ -1,62 +1,107 @@
 $(function() {
 
-  var planes = [
-      ["Nut Free",43.653440, -79.38409],
-      ["Gluten Free",43.662892, -79.395656],
-      ["Special chef requests available",43.657658, -79.378802],
-      ["Allergen specialty bakery",43.670002, -79.334793],
-      ["Centre of allergies",43.641438, -79.389353],
-      ["Loving hut - vegan",43.647784,-79.396534],
-      ["FEAST - healthy groceries",43.645505,-79.411468]
-  ];
+  var mbUrl = 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png';
 
-  /*
-  // NEW ---------------------------------------------------------
+  var streets  = L.tileLayer(mbUrl, {id: 'examples.map-i875mjb7'});
 
-  var starbucks = L.marker([43.653440, -79.38409]).bindPopup('Starbucks'),
-    burger    = L.marker([43.662892, -79.395656]).bindPopup('Burger Preists'),
-    feast    = L.marker([43.657658, -79.378802]).bindPopup('FEAST'),
-    ilFornello    = L.marker([43.670002, -79.334793]).bindPopup('Il Fornello');
+  var map = L.map('map', {
+    center: [43.6582713, -79.3777061],
+    zoom: 13,
+    layers: [streets]
+  });
 
-
-  var restaurants = L.layerGroup([starbucks, burger, feast, ilFornello]);
-
-  var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution}),
-      streets   = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution});
-
-
-
-
-
-  //  ---------------------------------------------------------
-  */
-
-  var map = L.map('map', {attributionControl: false}).setView([
-    43.6582713, // default lat
-    -79.3777061 // default lon
-    ], 13); // default zoom level
-
-  L.tileLayer('http://{s}.tiles.mapbox.com/v3/gabriel-florit.map-s24tp6w4/{z}/{x}/{y}.png', {
-    minZoom: 7,
-    maxZoom: 15
-  }).addTo(map);
-
-  for (var i = 0; i < planes.length; i++) {
-    marker = new L.marker([planes[i][1],planes[i][2]])
-      .bindPopup(planes[i][0])
-      .addTo(map);
-  }
-
+  var thekeg = new L.marker([43.6582713, -79.3777061]);
+  var veganhut = new L.marker([43.662892, -79.395656]);
   
+  var moutard = new L.marker([43.6782713, -79.3737061]);
+  var bigyellow = new L.marker([43.652892, -79.375656]);
 
+  var rubber = new L.marker([43.6770713, -79.3557061]);
+  var tofuuuu = new L.marker([43.608892, -79.321656]);
+
+  var thesea = new L.marker([43.6522713, -79.3177061]);
+  var ahoy = new L.marker([43.661892, -79.375656]);
+
+  var cluck = new L.marker([43.6552713, -79.3774061]);
+  var earlyrooster = new L.marker([43.672892, -79.315656]);
+
+  var cluck = new L.marker([43.6552713, -79.3774061]);
+  var earlyrooster = new L.marker([43.672892, -79.315656]);
+
+  var milkbarn = new L.marker([43.6532713, -79.3771061]);
+  var moo = new L.marker([43.612892, -79.393656]);
+
+  var treenut = L.layerGroup([thekeg, veganhut]);
+  var mustard = L.layerGroup([moutard, bigyellow]);
+  var soy = L.layerGroup([rubber, tofuuuu]);
+  var shellfish = L.layerGroup([thesea, ahoy]);
+  var wheat = L.layerGroup([cluck, earlyrooster]);
+  var peanut = L.layerGroup([cluck, earlyrooster]);
+  var dairy = L.layerGroup([milkbarn, moo]);
+
+
+  var all = L.layerGroup([treenut, mustard, soy, shellfish, wheat, peanut, dairy]);
+
+  map.addLayer( all );
 
   $('#addAllergy').click(function(){
-    console.log('works');
-    $('<select><option disabled selected> -- select an option -- </option><option value="Tree nuts">Tree nuts</option><option value="Peanuts">Peanuts</option><option value="Dairy">Dairy</option><option value="Shellfish">Shellfish</option></option><option value="Mustard">Mustard</option><option value="Soy">Soy</option><option value="Wheat">Wheat</option></select>').appendTo("section");
-    $('select').selectric('init');
+    $('<select name="food"><option disabled selected> -- select an option -- </option><option value="treenut">Tree nuts</option><option value="peanut">Peanuts</option><option value="dairy">Dairy</option><option value="shellfish">Shellfish</option></option><option value="mustard">Mustard</option><option value="soy">Soy</option><option value="Wheat">wheat</option></select>').appendTo("section");
+    $('select').selectric({
+      // when the drop down closes after selecting an item, grab the value of the item selected
+      onClose: function(element) {
+
+        var selectedItem = $(this).val();
+        //console.log(selectedItem);
+        //map.addLayer( selectedItem );
+        //console.log(element);
+
+        // if value of dropdown is for treenuts
+        if( selectedItem === "treenut" ){
+          map.removeLayer( treenut );
+          console.log("treeeeeeeenuuuuttttt");
+        }
+
+        // if value of dropdown is for peanuts
+        if( selectedItem === "peanut" ){
+          map.removeLayer( peanut );
+          console.log('pppppeeeeeaaaaannnnnnuuuuuttt');
+        }
+
+        // if value of dropdown is for dairy
+        if( selectedItem === "dairy" ){
+          map.removeLayer( dairy );
+          console.log('daaaaaaiiiiirrrryyyy');
+        }
+
+        // if value of dropdown is for shellfish
+        if( selectedItem === "shellfish" ){
+          map.removeLayer( shellfish );
+          console.log('sheeeeeeellllllfffffiiiisssshh');
+        }
+
+        // if value of dropdown is for mustard
+        if( selectedItem === "mustard" ){
+          map.removeLayer( mustard );
+          console.log('muuuuuussssttttaaaaarrrd');
+        }
+
+        // if value of dropdown is for soy
+        if( selectedItem === "soy" ){
+          map.removeLayer( soy );
+          console.log('sssssssoooooooyyyyyy');
+        }
+
+        // if value of dropdown is for wheat
+        if( selectedItem === "wheat" ){
+          map.removeLayer( wheat );
+          console.log('wwwhhhheeeaaattt');
+        }
+
+
+
+      }
+    });
   });
 
 
 });
-
-
